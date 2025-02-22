da5f649f25d84ceee850db67890c996a7a0efa25
Travis Bubb <travisbubb14@gmail.com>
2025-02-22 16:14:37 -0500

    feat: Create a simple server that listens to a Unix domain socket

--- crates/docksterd/src/main.rs
+++ crates/docksterd/src/main.rs
@@ -1,3 +1,47 @@
+use std::{
+    fs::{exists, remove_file},
+    io::Read,
+    os::unix::net::{UnixListener, UnixStream},
+    process::exit,
+    thread,
+};
+
 fn main() {
-    println!("Hello, world!");
+    const SOCKET_PATH: &str = "/tmp/docksterd.socket";
+
+    if exists(SOCKET_PATH).expect("Error checking if socket exists") {
+        if let Err(error) = remove_file(SOCKET_PATH) {
+            println!("Error removing socket {}: {}", SOCKET_PATH, error);
+            exit(1);
+        }
+    }
+
+    let listener = match UnixListener::bind(SOCKET_PATH) {
+        Ok(listener) => listener,
+        Err(error) => {
+            println!("Error binding socket: {}", error);
+            exit(1);
+        }
+    };
+
+    loop {
+        match listener.accept() {
+            Ok((sock, _)) => {
+                thread::spawn(|| handle_client(sock));
+            }
+            Err(error) => {
+                println!("Failed to accept incoming connection: {}", error);
+            }
+        }
+    }
+}
+
+fn handle_client(mut stream: UnixStream) {
+    println!("handling connection for client");
+
+    let mut buf = String::new();
+    match stream.read_to_string(&mut buf) {
+        Ok(num_bytes) => println!("Read {} bytes: {}", num_bytes, buf),
+        Err(error) => println!("Error reading from socket: {}", error),
+    }
 }
